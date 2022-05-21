c3020517dbbd0e68f1022ddc9c292c81341cb2b9
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2022-05-21 16:50:26 +0000

    up

---
+++ src/main.rs
@@ -0,0 +1,209 @@
+#![allow(unused_imports)]
+#![allow(clippy::single_component_path_imports)]
+
+#[allow(dead_code)]
+#[cfg(not(feature = "qemu"))]
+const SSID: &str = env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
+#[allow(dead_code)]
+#[cfg(not(feature = "qemu"))]
+const PASS: &str = env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
+
+#[allow(dead_code)]
+mod test;
+
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::path::PathBuf;
+use std::sync::{Condvar, Mutex};
+use std::{cell::RefCell, env, sync::atomic::*, sync::Arc, thread, time::*};
+
+use anyhow::bail;
+
+use embedded_svc::mqtt::client::utils::ConnState;
+use log::*;
+
+use url;
+
+use smol;
+
+use embedded_hal::adc::OneShot;
+use embedded_hal::blocking::delay::DelayMs;
+use embedded_hal::digital::v2::OutputPin;
+
+use embedded_svc::eth;
+use embedded_svc::eth::{Eth, TransitionalState};
+use embedded_svc::httpd::registry::*;
+use embedded_svc::httpd::*;
+use embedded_svc::io;
+use embedded_svc::ipv4;
+use embedded_svc::mqtt::client::{Client, Connection, MessageImpl, Publish, QoS};
+use embedded_svc::ping::Ping;
+use embedded_svc::sys_time::SystemTime;
+use embedded_svc::timer::TimerService;
+use embedded_svc::timer::*;
+use embedded_svc::wifi::*;
+
+use esp_idf_svc::eth::*;
+use esp_idf_svc::eventloop::*;
+use esp_idf_svc::eventloop::*;
+use esp_idf_svc::httpd as idf;
+use esp_idf_svc::httpd::ServerRegistry;
+use esp_idf_svc::mqtt::client::*;
+use esp_idf_svc::netif::*;
+use esp_idf_svc::nvs::*;
+use esp_idf_svc::ping;
+use esp_idf_svc::sntp;
+use esp_idf_svc::sysloop::*;
+use esp_idf_svc::systime::EspSystemTime;
+use esp_idf_svc::timer::*;
+use esp_idf_svc::wifi::*;
+
+use esp_idf_hal::adc;
+use esp_idf_hal::delay;
+use esp_idf_hal::gpio;
+use esp_idf_hal::i2c;
+use esp_idf_hal::prelude::*;
+use esp_idf_hal::spi;
+
+use esp_idf_sys::{self, c_types};
+use esp_idf_sys::{esp, EspError};
+
+use display_interface_spi::SPIInterfaceNoCS;
+
+use embedded_graphics::mono_font::{ascii::FONT_10X20, MonoTextStyle};
+use embedded_graphics::pixelcolor::*;
+use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::*;
+use embedded_graphics::text::*;
+
+use ili9341;
+use ssd1306;
+use ssd1306::mode::DisplayConfig;
+use st7789;
+
+use epd_waveshare::{epd4in2::*, graphics::VarDisplay, prelude::*};
+
+fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
+    info!("About to do some pings for {:?}", ip_settings);
+
+    let ping_summary =
+        ping::EspPing::default().ping(ip_settings.subnet.gateway, &Default::default())?;
+    if ping_summary.transmitted != ping_summary.received {
+        bail!(
+            "Pinging gateway {} resulted in timeouts",
+            ip_settings.subnet.gateway
+        );
+    }
+
+    info!("Pinging done");
+
+    Ok(())
+}
+
+fn wifi(
+    netif_stack: Arc<EspNetifStack>,
+    sys_loop_stack: Arc<EspSysLoopStack>,
+    default_nvs: Arc<EspDefaultNvs>,
+) -> Result<Box<EspWifi>> {
+    let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
+
+    info!("Wifi created, about to scan");
+
+    let ap_infos = wifi.scan()?;
+
+    let ours = ap_infos.into_iter().find(|a| a.ssid == SSID);
+
+    let channel = if let Some(ours) = ours {
+        info!(
+            "Found configured access point {} on channel {}",
+            SSID, ours.channel
+        );
+        Some(ours.channel)
+    } else {
+        info!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            SSID
+        );
+        None
+    };
+
+    wifi.set_configuration(&Configuration::Mixed(
+        ClientConfiguration {
+            ssid: SSID.into(),
+            password: PASS.into(),
+            channel,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: "aptest".into(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
+
+    info!("Wifi configuration set, about to get status");
+
+    wifi.wait_status_with_timeout(Duration::from_secs(20), |status| !status.is_transitional())
+        .map_err(|e| anyhow::anyhow!("Unexpected Wifi status: {:?}", e))?;
+
+    let status = wifi.get_status();
+
+    if let Status(
+        ClientStatus::Started(ClientConnectionStatus::Connected(ClientIpStatus::Done(ip_settings))),
+        ApStatus::Started(ApIpStatus::Done),
+    ) = status
+    {
+        info!("Wifi connected");
+
+        ping(&ip_settings)?;
+    } else {
+        bail!("Unexpected Wifi status: {:?}", status);
+    }
+
+    Ok(wifi)
+}
+
+pub struct ControlData {
+    pub offset: i32,
+}
+
+pub struct StateData {}
+
+
+pub struct Workspace<GpioA: gpio::InputPin, GpioB: gpio::InputPin, GpioC: gpio::InputPin> {
+    // 3 input pins
+    pub recv_a: GpioA,
+    pub recv_b: GpioB,
+    pub recv_c: GpioC,
+}
+
+
+fn main() -> Result<()> {
+    esp_idf_sys::link_patches();
+
+
+    let peripherals = Peripherals::take().unwrap();
+    let pins = peripherals.pins;
+
+    let netif_stack = Arc::new(EspNetifStack::new()?);
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
+    let default_nvs = Arc::new(EspDefaultNvs::new()?);
+
+
+    let mut wifi = wifi(
+        netif_stack.clone(),
+        sys_loop_stack.clone(),
+        default_nvs.clone(),
+    )?;
+
+    let workspace = Workspace {
+        recv_a: pins.gpio4.into_input()?,
+        recv_b: pins.gpio9.into_input()?,
+        recv_c: pins.gpio8.into_input()?,
+    };
+
+
+    Ok(())
+}
+
